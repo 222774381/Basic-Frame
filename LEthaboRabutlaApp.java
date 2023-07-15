ca381c6feaee8b85bdb13f18e3f85a89a800c284
222774381 <139621723+222774381@users.noreply.github.com>
2023-07-15 12:12:42 +0200

    Add files via upload

---
+++ LEthaboRabutlaApp.java
@@ -0,0 +1,15 @@
+package lethaborabutlaapp;
+
+import me.thabo.rabutla.myClass;
+
+public class LEthaboRabutlaApp {
+
+    public static void main(String[] args) {
+        
+        myClass m;
+        
+        m = new myClass();
+        
+    }
+
+}
